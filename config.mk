5a5c1af8feb53c729342c99896b614ab4b4aa625
cartilo-kun <63048078+cartilo-kun@users.noreply.github.com>
2022-01-18 16:35:42 +0800

    Add files via upload

--- config.mk
+++ config.mk
@@ -1,5 +1,7 @@
-# dmenu version
-VERSION = 5.0
+# st version
+VERSION = 0.8.4
+
+# Customize below to fit your system
 
 # paths
 PREFIX = /usr/local
@@ -8,37 +10,43 @@ MANPREFIX = $(PREFIX)/share/man
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
-# Xinerama, comment if you don't want it
-XINERAMALIBS  = -lXinerama
-XINERAMAFLAGS = -DXINERAMA
-
-# freetype
-FREETYPELIBS = -lfontconfig -lXft
-FREETYPEINC = /usr/include/freetype2
-# OpenBSD (uncomment)
-#FREETYPEINC = $(X11INC)/freetype2
-
-# Uncomment this for the json patch / JSON_PATCH
-#JANSSONINC = `pkg-config --cflags jansson`
-#JANSSONLIBS = `pkg-config --libs jansson`
-# uncomment on RHEL for strcasecmp
-#EXTRAFLAGS=-D_GNU_SOURCE
+PKG_CONFIG = pkg-config
 
 # Uncomment this for the alpha patch / ALPHA_PATCH
 XRENDER = -lXrender
 
-# Uncomment for the pango patch / PANGO_PATCH
-#PANGOINC = `pkg-config --cflags xft pango pangoxft`
-#PANGOLIB = `pkg-config --libs xft pango pangoxft`
+# Uncomment this for the themed cursor patch / THEMED_CURSOR_PATCH
+#XCURSOR = -lXcursor
 
-# includes and libs
-INCS = -I$(X11INC) -I$(FREETYPEINC) $(JANSSONINC) ${PANGOINC}
-LIBS = -L$(X11LIB) -lX11 $(XINERAMALIBS) $(FREETYPELIBS) $(JANSSONLIBS) -lm $(XRENDER) ${PANGOLIB}
+# Uncomment the lines below for the ligatures patch / LIGATURES_PATCH
+#LIGATURES_C = hb.c
+#LIGATURES_H = hb.h
+#LIGATURES_INC = `$(PKG_CONFIG) --cflags harfbuzz`
+#LIGATURES_LIBS = `$(PKG_CONFIG) --libs harfbuzz`
+
+# Uncomment this for the SIXEL patch / SIXEL_PATCH
+#SIXEL_C = sixel.c sixel_hls.c
+
+# includes and libs, uncomment harfbuzz for the ligatures patch
+INCS = -I$(X11INC) \
+       `$(PKG_CONFIG) --cflags fontconfig` \
+       `$(PKG_CONFIG) --cflags freetype2` \
+       $(LIGATURES_INC)
+LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft ${XRENDER} ${XCURSOR}\
+       `$(PKG_CONFIG) --libs fontconfig` \
+       `$(PKG_CONFIG) --libs freetype2` \
+       $(LIGATURES_LIBS)
 
 # flags
-CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 -D_POSIX_C_SOURCE=200809L -DVERSION=\"$(VERSION)\" $(XINERAMAFLAGS) $(EXTRAFLAGS)
-CFLAGS   = -std=c99 -pedantic -Wall -Os $(INCS) $(CPPFLAGS)
-LDFLAGS  = $(LIBS)
+STCPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600
+STCFLAGS = $(INCS) $(STCPPFLAGS) $(CPPFLAGS) $(CFLAGS)
+STLDFLAGS = $(LIBS) $(LDFLAGS)
+
+# OpenBSD:
+#CPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600 -D_BSD_SOURCE
+#LIBS = -L$(X11LIB) -lm -lX11 -lutil -lXft \
+#       `pkg-config --libs fontconfig` \
+#       `pkg-config --libs freetype2`
 
 # compiler and linker
-CC = cc
+# CC = c99
